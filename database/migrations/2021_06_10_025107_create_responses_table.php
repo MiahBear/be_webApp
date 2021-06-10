f17f40c004626db98f2e4d8413177e277f6c5f62
Pimentel <Jeremiah.Pimentel@megaxcess.com>
2021-06-10 11:30:02 +0800

    initial BE for web app

---
+++ database/migrations/2021_06_10_025107_create_responses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->string('products', 255);
+            $table->timestamp('date_and_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+}
